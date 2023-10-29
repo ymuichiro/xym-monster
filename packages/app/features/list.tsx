77874cea82e5a8cda9dc9bfb649d4b37ffbaa865
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-10-30 01:25:26 +0900

    [add] 説明文をモンスターリストより削除、モンスター画像がカードを超過しないように修正

--- packages/app/features/list.tsx
+++ packages/app/features/list.tsx
@@ -1,6 +1,7 @@
-import { Button, Card, H1, H2, Paragraph, ScrollView, XStack, YStack } from '@my/ui';
+import { Button, Card, H1, H2, Paragraph, ScrollView, SheetBase, XStack, YStack } from '@my/ui';
 import { Image } from '@tamagui/image';
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { Dimensions } from 'react-native';
 
 interface MonsterProps {
   payload: string;
@@ -10,7 +11,6 @@ interface Monsters {
   no: string; // モンスター No
   reality: Reality; // モンスター のレア度
   name: string; // モンスター 名
-  description: string; // モンスター 説明
   href: string; // モンスター 画像のURL ※ 画像データそのものだとちょっと重い
   isHas: boolean; // 自身がそのモンスターを持っているか否か
 }
@@ -23,13 +23,13 @@ function getRandomRarity() {
   return rarities[index];
 }
 
-const DUMMY_RESPONSE: Monsters[] = new Array(1000).fill(null).map((_, index) => {
+const DUMMY_RESPONSE: Monsters[] = new Array(20).fill(null).map((_, index) => {
+  const isHas: boolean = Math.random() < 0.5;
   return {
     no: `${index}`,
-    name: `Monster ${index}`,
-    description: `This is a description for Monster ${index}`,
-    href: `/twitter-card.png`, // 一旦ダミー画像
-    isHas: Math.random() < 0.5,
+    name: isHas ? `Monster ${index}` : '????',
+    href: isHas ? '/twitter-card.png' : '/egg.png', // 一旦ダミー画像
+    isHas: isHas,
     reality: getRandomRarity(),
   } as Monsters;
 });
@@ -43,6 +43,7 @@ const DUMMY_RESPONSE: Monsters[] = new Array(1000).fill(null).map((_, index) => 
 export function MonstersList(props: MonsterProps) {
   const [monsters, setMonsters] = useState<Monsters[]>([]);
   const [reality, setReality] = useState<Reality>('common');
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
 
   // モンスターの情報をサーバーより取得する
   const handleGetMonsters = async () => {
@@ -94,7 +95,7 @@ export function MonstersList(props: MonsterProps) {
         What will happen to those who collect all the XYM Monsters? Now, collect them!
       </Paragraph>
       {/* actions panel */}
-      <XStack justifyContent="center" space={'$4'} py={'$4'}>
+      <XStack justifyContent="center" flexWrap="wrap" gap={'$4'} py={'$4'}>
         {(['common', 'rare', 'epic', 'legendary'] as Reality[]).map((item, index) => (
           <Button themeInverse={reality == item} key={index} onPress={() => handleSelectReality(item)}>
             {item}
@@ -108,49 +109,97 @@ export function MonstersList(props: MonsterProps) {
           .map((item, index) => (
             <XStack
               key={index}
+              width={'100%'}
               $gtXs={{ width: '100%' }}
               $gtSm={{ width: '50%' }}
               $gtMd={{ width: '30%' }}
               $gtLg={{ width: '20%' }}
               padding={10}
             >
-              <Card
-                elevate
-                animation="bouncy"
-                scale={1}
-                hoverStyle={{ scale: 0.925 }}
-                pressStyle={{ scale: 0.875 }}
-                width={'100%'}
-                bordered
-                opacity={item.isHas ? 1 : 0.2}
-                disabled={!item.isHas}
-                {...props}
-              >
-                <Card.Header padded>
-                  <Image
-                    resizeMode="contain"
-                    alignSelf="center"
-                    source={{
-                      width: 300,
-                      height: 300,
-                      uri: item.href,
-                    }}
-                  />
-                </Card.Header>
-                <Card.Footer paddingLeft={'$4'} paddingRight={'$4'} paddingBottom={'$2'}>
-                  <YStack maxWidth={'100%'}>
-                    <Paragraph theme="alt2">{`No ${item.no.toString()}`}</Paragraph>
-                    <H2>{item.name}</H2>
-                    <Paragraph theme="alt1" textOverflow="ellipsis" minHeight={100}>
-                      {item.description.length >= 100 ? `${item.description.slice(0, 100)}...` : item.description}
-                    </Paragraph>
-                    <Paragraph theme="alt2">{item.isHas ? 'availability' : 'unavailable'}</Paragraph>
-                  </YStack>
-                </Card.Footer>
-              </Card>
+              <MonsterCard {...item} onPressHandle={() => setCurrentImage(item.href)} />
             </XStack>
           ))}
       </XStack>
+      {!!currentImage && (
+        <SheetBase isOpen={!!currentImage} onOpenChange={() => setCurrentImage(null)}>
+          <ViewModal href={currentImage} />
+        </SheetBase>
+      )}
     </ScrollView>
+  );
+}
+
+function MonsterCard(item: Monsters & { onPressHandle: () => void }): JSX.Element {
+  const [width, setWidth] = useState<number>(100);
+
+  return (
+    <Card
+      elevate
+      animation="bouncy"
+      scale={1}
+      hoverStyle={{ scale: 0.925 }}
+      pressStyle={{ scale: 0.875 }}
+      width={'100%'}
+      bordered
+      opacity={item.isHas ? 1 : 0.2}
+      disabled={!item.isHas}
+      onLayout={(e) => setWidth(e.nativeEvent.layout.width)}
+      onPress={item.onPressHandle}
+      padding={0}
+    >
+      <Card.Header padding={0}>
+        <Image
+          resizeMode="contain"
+          alignSelf="center"
+          source={{
+            width: width < 300 ? width : 300,
+            height: 300,
+            uri: item.href,
+          }}
+        />
+      </Card.Header>
+      <Card.Footer paddingLeft={'$4'} paddingRight={'$4'} paddingBottom={'$2'}>
+        <YStack maxWidth={'100%'}>
+          <Paragraph theme="alt2">{`No ${item.no.toString()}`}</Paragraph>
+          <H2>{item.name}</H2>
+          <Paragraph theme="alt2">{item.isHas ? 'availability' : 'unavailable'}</Paragraph>
+        </YStack>
+      </Card.Footer>
+    </Card>
+  );
+}
+
+interface ViewModalProps {
+  href: string;
+}
+
+function ViewModal(props: ViewModalProps): JSX.Element {
+  const [width, setWidth] = useState<number>(Dimensions.get('window').width);
+  const [height, setHeight] = useState<number>(Dimensions.get('window').height);
+
+  useEffect(() => {
+    const _wevent = Dimensions.addEventListener('change', (e) => {
+      setWidth(e.window.width);
+    });
+    const _hevent = Dimensions.addEventListener('change', (e) => {
+      setHeight(e.window.height);
+    });
+    return () => {
+      _wevent.remove();
+      _hevent.remove();
+    };
+  }, []);
+
+  return (
+    <YStack>
+      <Image
+        resizeMode="contain"
+        alignSelf="center"
+        width="100%"
+        height="100%"
+        maxWidth={800}
+        source={{ uri: props.href, width: width, height: height / 2 }}
+      />
+    </YStack>
   );
 }
